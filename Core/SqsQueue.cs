a9c4c7bd3b5df6ae4cc23fae143b017976ed3ca5
Tim Lange <Tim.lange@park-now.com>
2022-06-26 13:22:16 +0200

    Moved project

---
+++ Core/SqsQueue.cs
@@ -0,0 +1,55 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using System.Threading.Tasks;
+
+namespace sqs_handler
+{
+    internal class SqsQueue
+    {
+        public string Role { get; set; }
+        public string AccountId { get; set; }
+
+        public SqsQueue(string env)
+        {
+            //roles
+            if (env == "Phonixx") Role = "pmgroup-prod";
+            else Role = "parknow-bloxx-prod";
+
+            //AWS Account id's
+            if (env == "Phonixx") AccountId = "660620967782";
+            else AccountId = "680226270606";
+
+        }
+
+        public static RegionEndpoint GetRegionEndpoint(string region)
+        {
+            //AWS RegionEndpoints
+            switch (region)
+            {
+                case "eu-central-1":
+                    return RegionEndpoint.EUCentral1;
+                case "eu-west-1":
+                    return RegionEndpoint.EUWest1;
+                case "eu-west-2":
+                    return RegionEndpoint.EUWest2;
+                case "eu-south-1":
+                    return RegionEndpoint.EUSouth1;
+                default:
+                    return RegionEndpoint.EUNorth1;
+            }
+        }
+
+        //Receives the messages from queue in batches of 10
+        public static async Task<ReceiveMessageResponse> GetMessagesAsync(
+            IAmazonSQS sqsClient, string qUrl)
+        {
+            return await sqsClient.ReceiveMessageAsync(new ReceiveMessageRequest
+            {
+                QueueUrl = qUrl,
+                MaxNumberOfMessages = 10,
+                VisibilityTimeout = 20
+            });
+        }
+    }
+}
